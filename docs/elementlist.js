91e6917e880a2cd3c7be7607ac2735d338496642
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-18 12:37:38 +0300

    upgrade docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Deimos\\Controller\\Controller"],["c","Deimos\\Controller\\Exceptions\\ControllerNotFound"],["c","Deimos\\Controller\\Exceptions\\DisplayNone"],["c","Deimos\\Controller\\Exceptions\\RequestNotFound"],["c","Deimos\\Controller\\Proxy"],["c","Deimos\\Controller\\Runner"],["c","Deimos\\Controller\\Traits\\Request"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
+ApiGen.elements = [["c","Deimos\\Controller\\Controller"],["c","Deimos\\Controller\\Exceptions\\ControllerNotFound"],["c","Deimos\\Controller\\Exceptions\\DisplayNone"],["c","Deimos\\Controller\\Exceptions\\RequestNotFound"],["c","Deimos\\Controller\\Processor"],["c","Deimos\\Controller\\Proxy"],["c","Deimos\\Controller\\Traits\\Request"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
